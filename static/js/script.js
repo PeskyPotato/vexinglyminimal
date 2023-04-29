19fe2a0ea3b7df826364c1ffc42876f1f1e0c5be
PeskyPotato <peskypotato@protonmail.com>
2023-04-29 15:31:03 +0200

    Copy code to clipboard (#2)

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,17 @@
+function copyToClipboard(e) {
+    navigator.clipboard.writeText(e.nextSibling.getElementsByTagName("code")[0].innerText);
+    e.innerText = "Copied!";
+}
+
 window.addEventListener('load', function() {
     document.querySelectorAll('pre > code').forEach(function (codeBlock) {
-        console.log(codeBlock)
         var button = document.createElement('button');
         button.className = 'copy-code-button';
         button.type = 'button';
         button.innerText = 'Copy';
+        button.onclick = function() {
+            copyToClipboard(button);
+        }
 
         var pre = codeBlock.parentNode;
         if (pre.parentNode.classList.contains('highlight')) {
